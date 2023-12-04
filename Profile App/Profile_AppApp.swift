f2ec7646c348286d461bb27da5fd791671e55b6b
alexisbarros <apenasalexis@gmail.com>
2023-12-04 16:29:25 -0300

    Initial Commit

---
+++ Profile App/Profile_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Profile_AppApp.swift
+//  Profile App
+//
+//  Created by Alexis Barros on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Profile_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
