1c814fc76d7f2c469c77981e5d890edf19f67ac4
alexisbarros <apenasalexis@gmail.com>
2023-12-06 10:55:11 -0300

    :art: implements follow button

--- Profile App/ProfileView.swift
+++ Profile App/ProfileView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 //MARK: Model
 struct User {
+    var profilePhoto: String
     var name: String
     var github: String
     var follows: String
@@ -21,32 +22,38 @@ class ProfileModelView {
 
 //MARK: View
 struct ProfileView: View {
+    let isFollowing: Bool = false
+    
     var body: some View {
-        ZStack {
-            Rectangle().foregroundColor(.black)
-                .ignoresSafeArea()
+        VStack {
+            Image("profile_photo")
+                .resizable()
+                .aspectRatio(contentMode: .fit).clipShape(Capsule())
+                .frame(height: 300)
+                .padding(.bottom, 30)
             
-            VStack {
-                Image("profile_photo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit).clipShape(Capsule())
-                    .frame(height: 300)
-                    .padding(.bottom, 30)
-                
-                Text("Alexis de Barros")
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
-                
-                Text("github.com/alexisbarros")
-                    .font(.title2)
-                    .foregroundColor(.gray)
-                    .padding(.bottom)
-                
-                Text("10")
-                    .font(.system(size: 80))
-                    .foregroundColor(.white)
+            Text("Alexis de Barros")
+                .font(.largeTitle)
+            
+            Text("github.com/alexisbarros")
+                .font(.title2)
+                .foregroundColor(.gray)
+                .padding(.bottom)
+            
+            Text("10")
+                .font(.system(size: 80))
+            
+            Button {} label: {
+                Label(isFollowing ? "Unfollow" : "Follow", systemImage: "person.fill.badge.\(isFollowing ? "minus":"plus")")
+                    .frame(maxWidth: .infinity)
+                    .padding(10)
+                    .font(.title3)
             }
-        }
+            .buttonStyle(.bordered)
+            .tint(isFollowing ? .red : .blue)
+            
+                
+        }.padding()
     }
 }
 
